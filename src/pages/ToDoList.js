3b325d84f9dea6fd81af5721323b9dea5022221e
maciopelo <maciej.k00h@gmail.com>
2021-06-08 16:41:47 +0200

    add better styles in login and register panels

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
+import "../styles/ToDo.scss"
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux"
 import { fetchUserToDoLists } from "../redux/services"
 
+
+const parseDate = (date) =>{
+    const result = date.substr(0,10).split("-")
+    return `${result[2]}-${result[1]}-${result[0]}`
+}
 
 const ToDoList = () => {
 
@@ -16,19 +22,39 @@ const ToDoList = () => {
 
   
     return ( 
-        <div>
-             <h1>ToDoList</h1>
-             <h2>{email}</h2>
-             <h2>{login}</h2>
+        <div className="to-do-lists-container">
 
+            <menu className="to-do-lists-menu">
+                <input placeholder="Search"/>
+                <select placeholder="Search">
+                    <option value="sort-by">Sort by</option>
+                    <option value="by-date">Date</option>
+                    <option value="alph">Alpha</option>
+                    <option value="done-task">Done tasks</option>
+                </select>
+            </menu>
+
+            <ul className="to-do-lists">
              {todoLists.map(todo => {
+
+                console.log(todo)
+
+                const allTasks = todo.task.length
+                const doneTasks = todo.task.filter(task => task.isDone).length
+                const date = parseDate(todo.created_at)
+
                  return(
-                     <div>
+                     <li key={todo.id}>
+                         
                          <span>{todo.name}</span>
-                         <span>{todo.created_at}</span>
-                     </div>
+                         <span>{`Created at: ${date}`}</span>
+                         
+                         <span>{`Completed: ${doneTasks} Uncompleted: ${allTasks-doneTasks} All: ${allTasks-doneTasks}`}</span>
+                        
+                     </li>
                  )
-             })}
+                 })}
+            </ul>
         </div>
    
     );
